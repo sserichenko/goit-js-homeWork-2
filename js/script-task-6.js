c6bcb8ac794d871932288e30993bff7e65356b5c
sserichenko <sserichenko@gmail.com>
2019-10-06 20:34:17 +0300

    done hw 2

--- js/script-task-6.js
+++ js/script-task-6.js
@@ -2,15 +2,20 @@
 let total = 0;
 let input;
 const numbers = [];
+let sum = 0;
 
 while (input !== null) {
-  input = prompt('Введите произвольное число');
+  input = prompt("Введите произвольное число");
   if (!Number.isNaN(Number(input)) && Number(input) > 0) {
     let insNumber = Number(input);
-    console.log('insNumber :', insNumber);
-    total = total + insNumber;
+    console.log("insNumber :", insNumber);
+    numbers.push(insNumber);
+    console.log(numbers);  
   }
-  console.log(total);
-}
-alert(`Общая сумма чисел равна ${total}`);
 
+}
+for(const count of numbers){
+      total += count;
+    }
+
+alert(`Общая сумма чисел равна ${total}`);
